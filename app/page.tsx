c84b9d45570b4ba664c2ef8de0ad5c0cdb308904
fpole <137444215+fpole@users.noreply.github.com>
2024-02-16 16:51:35 +0000

    added message for when no gigs were found, made page more responsive to screen sizes

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [artistSearch, setArtistSearch] = useState("");
+  const [pagePopulated, setPagePopulated] = useState(false);
 
   async function handleSubmit() {
     //function to take input value and save variable artistSearch
@@ -14,15 +15,25 @@ export default function Home() {
       document.getElementById("searchInput") as HTMLInputElement
     ).value;
     setArtistSearch(artistName);
+    let reset = ((
+      document.getElementById("searchInput") as HTMLInputElement
+    ).value = "");
+    setPagePopulated(true);
   }
 
   return (
     <>
-      <div className="flex w-full max-w-sm m-3 items-center space-x-2">
-        <Input id="searchInput" type="search" placeholder="Search Artist" />
-        <Button type="submit" onClick={handleSubmit}>
-          Get Gigs!
-        </Button>
+      <div
+        className={`flex ${
+          pagePopulated ? "" : "h-screen"
+        } items-center justify-center w-full`}
+      >
+        <div className="flex max-w-md m-3 justify-center items-center space-x-2">
+          <Input id="searchInput" type="search" placeholder="Search Artist" />
+          <Button type="submit" onClick={handleSubmit}>
+            Get Gigs!
+          </Button>
+        </div>
       </div>
       <ArtistSearch artistSearch={artistSearch} />
       {/* <GigSearch /> */}
