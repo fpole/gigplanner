0c1173cc57a7e3db15510a62ec7f386f03d98c1c
fpole <137444215+fpole@users.noreply.github.com>
2024-03-15 18:13:11 +0000

    trying to fix deployment issue with localstorage not being defined error

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { Nav } from "@/components/Nav";
+import { useEffect } from "react";
 
 export default function Saved() {
-  let favourites = JSON.parse(localStorage.getItem("favourites") as string);
-  console.log(favourites);
+  useEffect(() => {
+    let favourites = JSON.parse(localStorage.getItem("favourites") as string);
+    console.log(favourites);
+  }, []);
 
   return (
     <>
